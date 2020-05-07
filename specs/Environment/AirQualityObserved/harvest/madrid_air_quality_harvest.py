90fe4d1164ea9e0deafc96f5b7ac3642167990a6
Fernando Lopez <fernando.lopez@fiware.org>
2020-05-07 17:18:39 +0200

    Correct flake8 problems

--- specs/Environment/AirQualityObserved/harvest/madrid_air_quality_harvest.py
+++ specs/Environment/AirQualityObserved/harvest/madrid_air_quality_harvest.py
@@ -127,6 +127,9 @@ class AirQualityObserved:
         self.dataset_url = \
             'https://datos.madrid.es/egob/catalogo/212531-7916318-calidad-aire-tiempo-real.txt'
 
+        self.dataset_stations = \
+            'https://datos.madrid.es/egob/catalogo/212629-1-estaciones-control-aire.csv'
+
         # Statistics for tracking purposes
         self.persisted_entities = 0
         self.in_error_entities = 0
@@ -157,7 +160,10 @@ class AirQualityObserved:
     def get_air_quality_madrid(self):
         """
         Header of the data
-        PROVINCIA	MUNICIPIO	ESTACION	MAGNITUD	PUNTO_MUESTREO	ANO	MES	DIA	H01	V01	H02	V02	H03	V03	H04	V04	H05	V05	H06	V06	H07	V07	H08	V08	H09	V09	H10	V10	H11	V11	H12	V12	H13	V13	H14	V14	H15	V15	H16	V16	H17	V17	H18	V18	H19	V19	H20	V20	H21	V21	H22	V22	H23	V23	H24	V24
+        PROVINCIA	MUNICIPIO	ESTACION	MAGNITUD	PUNTO_MUESTREO	ANO	MES	DIA
+        H01	V01	H02	V02	H03	V03	H04	V04	H05	V05	H06	V06	H07	V07	H08	V08	H09	V09	H10	V10
+        H11	V11	H12	V12	H13	V13	H14	V14	H15	V15	H16	V16	H17	V17	H18	V18	H19	V19	H20	V20
+        H21	V21	H22	V22	H23	V23	H24	V24
         """
         with closing(urlopen(url=self.dataset_url,
                              context=create_default_context(cafile=where())
